8c3bda31416e8d96b607e25611f0453fe3a53668
Naman Jain <jnamanjain2001@gmail.com>
2021-04-27 12:17:07 +0530

    Input Format added without git branch info

--- src/input.cpp
+++ src/input.cpp
@@ -7,13 +7,15 @@
 void print_input_format(){
 
     char hostname[50];
-
-    gethostname(hostname, sizeof(hostname));
     struct passwd *p = getpwuid(getuid());
 
-    printf("\033[;34m%s@", p->pw_name);
-    printf("%s\033[0m:", hostname);
+    // Get the host name and user name of current user
+    if(gethostname(hostname, sizeof(hostname)) == 0 && p){
+        printf("\033[;34m%s@", p->pw_name);
+        printf("%s\033[0m:", hostname);
+    }
 
+    // Print the current working directory of user
     char cwd[100];
     if (getcwd(cwd, sizeof(cwd)) != NULL) {
        printf("\033[;32m%s\033[0m₹ ", cwd);
