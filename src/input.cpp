f89ff4a92908945c78f29e8eb193e055c62d4b64
Naman Jain <jnamanjain2001@gmail.com>
2021-04-28 01:00:24 +0530

    Git Branch Status added
    merge this branch after merging batch mode and input formatting

--- src/input.cpp
+++ src/input.cpp
@@ -1,8 +1,10 @@
 #include <unistd.h>
 #include <limits.h>
-#include <limits.h>
 #include <pwd.h>
 #include <stdio.h>
+#include <string.h>
+
+void print_git_branch();
 
 void print_input_format(){
 
@@ -18,8 +20,36 @@ void print_input_format(){
     // Print the current working directory of user
     char cwd[100];
     if (getcwd(cwd, sizeof(cwd)) != NULL) {
-       printf("\033[;32m%s\033[0m₹ ", cwd);
+       printf("\033[;32m%s\033[0m", cwd);
     } else {
-       printf("\033[;32m.\033[0m₹ ");
+       printf("\033[;32m.\033[0m");
     }
+
+    // Get git branch if current directory is a git directory
+    print_git_branch();
+    printf("₹ ");
+}
+
+void print_git_branch(){
+    FILE *fp;
+    char path[400];
+    
+    // Get current branch and save its name in a file.
+    fp = popen("git branch --show-current 2>/dev/null", "r");
+
+    if (fp == NULL)
+        return;
+    
+    // Read branch name and print current branch name
+    fgets(path, 200, fp);
+    path[strcspn(path, "\n")] = 0;
+    
+    if(path && strcmp(path,"") == 0)
+        return;
+    
+    // print current branch name in red enclosed in round braces
+    printf(" \033[31m(%s)\033[0m",path);
+    
+    // Should check status if any error occurs
+    pclose(fp);
 }
