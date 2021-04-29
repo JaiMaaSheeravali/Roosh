e0630293e87b5584915597eaa4eaab391aa9fc40
Naman Jain <jnamanjain2001@gmail.com>
2021-04-29 14:54:37 +0530

    builtins improved

--- include/builtin.hpp
+++ include/builtin.hpp
@@ -1,9 +1,9 @@
 #pragma once
 #include <string>
 
-int roosh_cd(char **args);
-int roosh_history(char **args);
-int roosh_exit(char **args);
-int roosh_rsh(char **args);
+int roosh_cd(char **args, int num_args);
+int roosh_history(char **args, int num_args);
+int roosh_exit(char **args, int num_args);
+int roosh_rsh(char **args, int num_args);
 
 void push_command(std::string cmd);
