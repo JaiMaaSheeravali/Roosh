9159f975ebea716a98f1670e7c7fd8417cb8b583
ravjot <ravjotkaamra@gmail.com>
2021-05-06 15:30:26 +0530

    added: new_functions
    added check_builtin for checking whether the input command is a builtin
    added run_builtin for running the builtin after the check_builtin
    returns a index of the buitlin to be run

--- include/builtin.hpp
+++ include/builtin.hpp
@@ -1,9 +1,17 @@
 #pragma once
 #include <string>
+#include <vector>
 
+const std::vector<std::string> builtin_list{
+    "cd",
+    "history",
+    "exit",
+    "roosh"};
+
+int check_builtin(const std:: string &cmd);
+bool run_builtin(const std::string &cmd, int idx);
 int roosh_cd(char **args, int num_args);
 int roosh_history(char **args, int num_args);
 int roosh_exit(char **args, int num_args);
 int roosh_rsh(char **args, int num_args);
-
 void push_command(std::string cmd);
