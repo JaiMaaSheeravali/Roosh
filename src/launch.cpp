cff488249915d4512547136eb7ac14047578da75
ravjot <ravjotkaamra@gmail.com>
2021-05-06 15:58:47 +0530

    updated: pipes
    updateed to support pipes and redirections

--- src/launch.cpp
+++ src/launch.cpp
@@ -1,66 +1,27 @@
-#include <iostream>
-#include <cstring>
-#include <unistd.h>
-#include <sys/wait.h>
-#include "../include/builtin.hpp"
+#include "../include/launch.hpp"
+
+#include <string>
+#include <vector>
+
+#include "../include/parse.hpp"
+#include "../include/pipe.hpp"
+#include "../include/redirect.hpp"
 
 using namespace std;
 
-const char *builtin_list[] = {
-    "cd",
-    "history",
-    "exit",
-    "roosh"};
-
-int (*builtin_func[])(char **, int) = {
-    &roosh_cd,
-    &roosh_history,
-    &roosh_exit,
-    &roosh_rsh};
-
-bool roosh_exec(char **args)
+bool roosh_launch(const string &line)
 {
-    pid_t pid = 0;
-    pid = fork();
+    vector<string> cmds = roosh_tokenizer(line, '|');
 
-    if (pid < 0)
-    {
-        cerr << "fork failed\n";
-    }
-    else if (pid == 0)
-    {
-        // child process
-        if (execvp(args[0], args) == -1)
-        {
-            // kill the child process if an
-            // error occurs
-            cerr << "no such command exists\n";
-            _Exit(EXIT_FAILURE);
-        }
-    }
+    // take input for first command
+    // from a file if required
+    int in_fd = redirect_input(cmds[0]);
 
-    // parent process
-    wait(NULL);
-    return 1;
-}
-
-bool roosh_launch(char **args, int num_args)
-{
-    if (args[0] == NULL)
+    // if invalid file was mentioned just continue
+    if (in_fd == -1)
     {
         return 1;
     }
-
-    // built-in command
-    size_t builtin_len = sizeof(builtin_list) / sizeof(char *);
-    for (size_t i = 0; i < builtin_len; i++)
-    {
-        if (strcmp(builtin_list[i], args[0]) == 0)
-        {
-            return (*builtin_func[i])(args, num_args);
-        }
-    }
-
-    // linux kernel command
-    return roosh_exec(args);
+    
+    return pipe_exec(cmds, 0, in_fd);
 }
--- src/parse.cpp
+++ src/parse.cpp
@@ -1,9 +1,12 @@
-#include <iostream>
-#include <string>
-#include <sstream>
-#include <vector>
+#include "../include/parse.hpp"
+
+#include <algorithm>
 #include <cstring>
+#include <iostream>
+#include <sstream>
+#include <string>
 #include <utility>
+#include <vector>
 
 using namespace std;
 
@@ -18,6 +21,11 @@ std::pair<char **, int> roosh_parse(const string &line)
     vector<string> vs;
     while (iss >> word)
     {
+        if (word == "<" || word == ">" || word == ">>")
+        {
+            break;
+        }
+
         vs.push_back(word);
         n++;
     }
@@ -34,4 +42,48 @@ std::pair<char **, int> roosh_parse(const string &line)
     args[n] = NULL;
 
     return {args, n};
+}
+
+// trim from start
+static inline std::string &ltrim(std::string &s)
+{
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(),
+                                    std::not1(std::ptr_fun<int, int>(std::isspace))));
+    return s;
+}
+
+// trim from end
+static inline std::string &rtrim(std::string &s)
+{
+    s.erase(std::find_if(s.rbegin(), s.rend(),
+                         std::not1(std::ptr_fun<int, int>(std::isspace)))
+                .base(),
+            s.end());
+    return s;
+}
+
+// trim from both ends
+static inline std::string &trim(std::string &s)
+{
+    return ltrim(rtrim(s));
+}
+
+// return list of parameters of a command
+vector<string> roosh_tokenizer(const string &line, const char delimiter)
+{
+    vector<string> tokens;
+    string token;            // a single token
+    istringstream iss(line); // used to split string around delimiter
+
+    // Tokenizing w.r.t. specified delimiter
+    while (getline(iss, token, delimiter))
+    {
+        token = trim(token);
+        if (!token.empty())
+        {
+            tokens.push_back(token);
+        }
+    }
+
+    return tokens;
 }
