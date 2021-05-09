e0080523c3fac27eb85f5d3676c60bd0a37fca5f
guptaprakhariitr <prakharpguptag@gmail.com>
2021-05-09 12:57:15 +0530

    Transfer tutorial as build in function

--- include/builtin.hpp
+++ include/builtin.hpp
@@ -7,9 +7,10 @@ const std::vector<std::string> builtin_list{
     "history",
     "exit",
     "roosh",
-    "google"};
+    "google",
+    "tutorial"};
 
-int check_builtin(const std:: string &cmd);
+int check_builtin(const std::string &cmd);
 bool run_builtin(const std::string &cmd, int idx);
 int roosh_cd(char **args, int num_args);
 int roosh_history(char **args, int num_args);
--- src/launch.cpp
+++ src/launch.cpp
@@ -14,11 +14,6 @@ using namespace std;
 bool roosh_launch(const string &line)
 {
     vector<string> cmds = roosh_tokenizer(line, '|');
-    auto [args, num_args] = roosh_parse(line);
-    if (strcmp(args[0], "tutorial") == 0)
-    {
-        return roosh_exec_tutorial(args, num_args);
-    }
     // take input for first command
     // from a file if required
     int in_fd = redirect_input(cmds[0]);
